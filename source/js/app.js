fe84ea64ceb76fb4e712b47b16b7e45c097c77aa
Egor Korol <egor.korol@mail.ru>
2017-09-25 11:11:47 +0300

    add fullscreen menu, parallax scroll, flip(hover)

--- source/js/app.js
+++ source/js/app.js
@@ -1,1 +1,35 @@
 console.log('ggg');
+console.log($);
+
+
+// flipper welcome page
+$(document).ready(function () {
+  $('.autorization-btn').bind("click", function() {
+    $(this).next().toggleClass('hover');
+  })
+});
+
+
+
+//hamburger transform
+  // Look for .hamburger
+  var hamburger = document.querySelector(".hamburger");
+  // On click
+  hamburger.addEventListener("click", function() {
+    // Toggle class "is-active"
+    hamburger.classList.toggle("is-active");
+    hamburger.classList.add("close-button");
+    // Do something else, like open/close menu
+  });
+
+
+//full screen menu onclick
+  $(document).ready(function() {
+    $('.hamburger').on('click', function() {
+       $('.fsmenu').addClass('fsmenu--open');
+    });
+  
+    $('.close-button').on('click', function() {
+      $('.fsmenu').removeClass('fsmenu--open');
+    });
+  });
